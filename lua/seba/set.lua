300652c90743a5870c28de10c710b533a6b80e76
Ioannis Horgos <jhor@acta.com.gr>
2023-12-16 16:36:05 +0200

    new command for entering the normal mode

--- lua/seba/set.lua
+++ lua/seba/set.lua
@@ -6,11 +6,14 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.updatetime = 50
--- vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "80"
 
 vim.opt.incsearch = true
 vim.opt.hlsearch = false
 
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true})
+vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true})
+vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true})
 -- vim.api.nvim_set_keymap('i', '"', '""<left>', { noremap = true})
 -- vim.api.nvim_set_keymap('i', "'", "''<left>", { noremap = true})
 -- vim.api.nvim_set_keymap('i', '{', '{}<left><enter><Esc>O', { noremap = true})
